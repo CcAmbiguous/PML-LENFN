4dfcfa6e7990fe022fde310919739126a7d1df9d
Glad  Yu <124881634+CcAmbiguous@users.noreply.github.com>
2025-12-03 13:42:16 +0800

    Update dmeo.m

--- dmeo.m
+++ dmeo.m
@@ -6,10 +6,13 @@ num_noise = 1;% The amount of noise added
 % At present, there are two ways to add noise
 % the first is to randomly add a certain amount of noise, the second is the average random noise, that is avg_cls.
 % ttt = 1 the first way to add noise  
-% ttt = 1 the second way to add noise
+% ttt = 2 the second way to add noise
 ttt = 1;  %Select your noise method, which defaults to the first method of adding noise
 
+% you can use:
 [~] = main_PML(dataname,num_noise,ttt);
-% 
-% [~] = main_PML('emotions',1,ttt);
+% or:
+[~] = main_PML('emotions',1,ttt);
 % [~] = main_PML('emotions',2,ttt);
+
+
